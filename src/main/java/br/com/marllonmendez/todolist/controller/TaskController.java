e908bdf8f9a7cf18948055eee0cfc60d95eda126
Mendes <marlonmendesor@gmail.com>
2023-10-13 10:51:30 -0300

    add: added update method and null properties

--- src/main/java/br/com/marllonmendez/todolist/controller/TaskController.java
+++ src/main/java/br/com/marllonmendez/todolist/controller/TaskController.java
@@ -2,6 +2,7 @@ package br.com.marllonmendez.todolist.controller;
 
 import br.com.marllonmendez.todolist.model.TaskModel;
 import br.com.marllonmendez.todolist.repository.ITaskRepository;
+import br.com.marllonmendez.todolist.utils.NullProperties;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -44,4 +46,20 @@ public class TaskController {
         var listTasks = this.taskRepository.findByIdUser((UUID) idUser);
         return listTasks;
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id) {
+        Optional<TaskModel> idTask = this.taskRepository.findById(id);
+        if (idTask.isPresent()) {
+            TaskModel task = idTask.get();
+            TaskModel updatedTask = this.taskRepository.save(task);
+            NullProperties.copyNotNullProperties(taskModel, task);
+            return ResponseEntity.status(HttpStatus.OK).body(updatedTask);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Task inexistente!");
+        }
+    }
 }
+
+
+
--- src/main/java/br/com/marllonmendez/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/marllonmendez/todolist/filter/FilterTaskAuth.java
@@ -24,7 +24,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
             throws ServletException, IOException {
 
         var servletPath = request.getServletPath();
-        if (servletPath.equals("/tasks/")) {
+        if (servletPath.startsWith("/tasks/")) {
 
             // Autenticação pegando usuário e senha descriptografada
             var auth = request.getHeader("Authorization");
